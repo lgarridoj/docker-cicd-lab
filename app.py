4522f567d19f26dee5ae2d2fa7f5a8aa04b1e650
Luis Garrido <luisgarrido@Luis-Garrido.local>
2025-05-13 15:20:20 -0400

    1 commit

--- app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def home(): return 'Aplicacion CI/CD con Docker github acción'
-if __name__ == '__main__': app.run(host='0.0.0.0', port=5000)
